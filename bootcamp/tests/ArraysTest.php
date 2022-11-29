23ac45718b0a014099746aa7c019f73a543f3f93
demo.proyect <sara255530@gmail.com>
2022-11-29 13:22:06 +0100

    first commit

---
+++ bootcamp/tests/ArraysTest.php
@@ -0,0 +1,43 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertEquals;
+
+
+final class ArraysTest extends TestCase
+{
+    public function testArrayIndexado1(): void
+    {
+        // Consigue la frase esperada utilizando índices del array y concatenación
+        $seasons = ["Primavera", "Verano", "Otoño,", "Invierno"]; 
+        $phrase = '';
+        
+        $expectedPhrase = "La estación que más me gusta es el Verano y la que menos es el Invierno";
+        assertEquals($expectedPhrase, $phrase);
+    }
+
+    public function testArrayAsociativo(): void
+    {
+        // Consigue el email del usuario utilizando la clave del array
+        $user = ['name' => 'Carlos', 'email' => 'carlos@correo.com', 'city' => 'Benalmádena']; 
+        $userEmail = $user['email']; 
+
+        assertEquals('carlos@correo.com', $userEmail);
+    }
+
+    public function testArrayMultidimensional(): void
+    {
+        // Consigue los datos que se piden en los asserts
+        $users = [
+          ['name' => 'Carlos', 'email' => 'carlos@correo.com', 'city' => 'Benalmádena'],
+          ['name' => 'Carmen', 'email' => 'carmen@correo.com', 'city' => 'Fuengirola'],
+          ['name' => 'Carmelo', 'email' => 'carmelo@correo.com', 'city' => 'Torremolinos'],
+          ['name' => 'Carolina', 'email' => 'carolina@correo.com', 'city' => 'Málaga'],
+        ]; 
+
+        assertEquals('carlos@correo.com', $users[0]['email']);
+        assertEquals('Torremolinos', $users[2]['city']);
+        assertEquals('Carmen', $users[1]['name']);
+    }
+
+}
